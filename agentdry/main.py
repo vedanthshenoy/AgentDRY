0190f11eceb6339c063d5cb1a84985e9c014afce
vedanthshenoy <vedantshenoy1@gmail.com>
2025-08-01 15:17:00 +0530

    chore: change to path of server file

--- agentdry/main.py
+++ agentdry/main.py
@@ -1,6 +1,6 @@
 from utils.append_to_server import create_tool_from_user_input, append_code_to_server_file
 
-def create_and_update_tool(query: str, server_file_path : str = r"C:\prass\agentdry\servers\mcp_server_autorestart.py"):
+def create_and_update_tool(query: str, server_file_path : str = r"C:\prass\agentdry\servers\log_mcp_server_autorestart.py"):
     code_string = create_tool_from_user_input(query)
     append_code_to_server_file(code_string, server_file_path)
     print("Server Updated !!!")
